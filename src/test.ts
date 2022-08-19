b908a925f289ecfc51bb318f44b9a7806b70dcbd
mystikxkemix <mystikxkemix@gmail.com>
2022-08-19 13:36:17 +0200

    Add script test

---
+++ src/test.ts
@@ -0,0 +1,3 @@
+import parseUrl from "./Parser/WuxiaWorldParser/parseUrl";
+
+parseUrl("https://www.wuxiaworld.com/novel/return-of-the-disaster-class-hero", () => {}).then((res) => console.log(res));
